3c7109c68de1b9c347c526db90633d700d433ef6
urushi <krasavaagadaok@gmail.com>
2024-12-27 11:30:28 +0300

    Код находится в .cs файлах.

---
+++ Lab1/Lab1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получаем значения из TextBox
+            string name = textBox1.Text;
+            string ageText = textBox2.Text;
+
+            // Проверка на пустые поля
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(ageText))
+            {
+                // Преобразуем возраст в число
+                if (int.TryParse(ageText, out int age))
+                {
+                    // Создаем объект класса Person
+                    Person person = new Person
+                    {
+                        Name = name,
+                        Age = age
+                    };
+
+                    // Отображаем данные объекта в Label3 и Label4
+                    label3.Text = "Name: " + person.Name;
+                    label4.Text = "Age: " + person.Age;
+                }
+                else
+                {
+                    // Если возраст не число
+                    label3.Text = "Invalid age. Please enter a valid number.";
+                    label4.Text = "";
+                }
+            }
+            else
+            {
+                // Если одно из полей пустое
+                label3.Text = "Please enter both name and age.";
+                label4.Text = "";
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab1
+{
+    internal class Person
+    {
+        // Поля класса
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {age}";
+        }
+    }
+}
